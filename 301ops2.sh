e79cbfff6b2b646d6962044b5ecae2518fdbe45c
PaulThomas83 <118229944+PaulThomas83@users.noreply.github.com>
2023-01-25 01:25:46 +0000

    update

--- 301ops2.sh
+++ 301ops2.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-ps aux
-
-echo "please enter a PID you want to kill"
-
-read PID
-
-for kill in $PID
-do
-sudo kill $PID
-done
-
-echo "$PID killed"
-
-#!/bin/bash
 
 # How to show time and date
 
