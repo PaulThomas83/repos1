390af8b950da20f291a2cae6b1bf33d0ca25b91b
PaulThomas83 <118229944+PaulThomas83@users.noreply.github.com>
2023-02-16 00:50:32 +0000

    update

---
+++ 301ops8.py
@@ -0,0 +1,28 @@
+# ops 301 challenge 8
+# Author Paul Thomas
+# Date 2-14-23
+
+#declare variables
+fruit_list = ["grapefruit", "apple", "banana", "cherry", "orange", "kiwi", "watermelom", "grape", "lemon", "date"]
+#declare funtion
+
+#main
+print("my list of fruit")
+print(fruit_list)
+
+#print the fourth element of the list
+print ("this is the 4th fruit on the list:")
+print(fruit_list[3])
+
+#print the sixth through tenth elements of the list
+print ("these are 6-10 on my fruit list")
+print(fruit_list[5:10])
+
+#change the value of the seventh element to "onion" and the print
+print("changing the 7th element to onion")
+fruit_list[7] = "onion"
+print(fruit_list)
+
+#end
+
+
